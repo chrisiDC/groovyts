d656a3b6c5a65fe13b0165e7d1a87c595753f387
christian huber <christian.huber@groovbybytes.at>
2017-01-15 16:18:48 +0100

    initial commit2

--- src/client/components/sidebar/tmp/index2.js
+++ src/client/components/sidebar/tmp/index2.js
@@ -3,16 +3,45 @@
 
     $(document).ready(function () {
 
+        var link = $("#selectedMenuItem").val();
+
+        var linkItem = $("a[href='"+link+"']").next();
+        linkItem.parents(".list-group.collapse").collapse("show");
+
         $('#sidebar .collapse').on('show.bs.collapse', function (event) {
 
             var id = $(event.currentTarget).attr("id");
-            $(event.currentTarget).parent().children(".in").not("#"+id).collapse("hide");
+            $(event.currentTarget).parent().children(".in").not("#" + id).collapse("hide");
         });
 
         $('#sidebar .collapse').on('hidden.bs.collapse', function (event) {
             $(event.currentTarget).find(".in").collapse("hide");
         });
 
+        $("#sidebar-mini a[href]").click(function () {
+            toggleSidebar("default")
+        });
+        $("#sidebar-toggle").click(function () {
+            toggleSidebar("mini")
+        });
+
 
     });
+
+    function toggleSidebar(which) {
+        if (which) localStorage.setItem("sidebar", which);
+
+        var sideBar = $("#sidebar").parent();
+        var sideBarMini = $("#sidebar-mini").parent();
+
+        if (localStorage.getItem("sidebar") === "default") {
+            sideBar.attr("style", "");
+            sideBarMini.attr("style", "");
+        }
+        else {
+            sideBar.attr("style", "display: none !important");
+            sideBarMini.attr("style", "display:inherit !important");
+        }
+    }
+
 }());
